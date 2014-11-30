f4067db5d4ff54af8bc8e197fc4b9312c3871b82
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2014-11-30 18:30:39 +0100

    Update Flat.php
    fixed spacing

--- app/code/community/Fballiano/HideEmptyCategories/Model/Catalog/Resource/Category/Flat.php
+++ app/code/community/Fballiano/HideEmptyCategories/Model/Catalog/Resource/Category/Flat.php
@@ -5,7 +5,7 @@ class Fballiano_HideEmptyCategories_Model_Catalog_Resource_Category_Flat extends
     {
         $nodes = parent::_loadNodes($parentNode, $recursionLevel, $storeId);
         foreach ($nodes as $node) {
-			if ($node->getChildrenCategories()) continue;
+            if ($node->getChildrenCategories()) continue;
             if ($node->getProductCount()) continue;
             unset($nodes[$node->getId()]);
         }
