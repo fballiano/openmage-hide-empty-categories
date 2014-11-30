b6773dd9f32facc662ea1d256d121a09b2b1e859
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2014-11-30 18:35:43 +0100

    Update Observer.php
    fixes issue #2

--- app/code/community/Fballiano/HideEmptyCategories/Model/Observer.php
+++ app/code/community/Fballiano/HideEmptyCategories/Model/Observer.php
@@ -8,6 +8,7 @@ class Fballiano_HideEmptyCategories_Model_Observer
         $collection = $observer->getEvent()->getCategoryCollection();
         foreach ($collection as $key => $item) {
             if ($item->getEntityTypeId() == 3) {
+                if ($item->getDisplayMode() == "PAGE") continue;
                 if ($item->getChildrenCount()) continue;
                 if ($item->getProductCount()) continue;
                 $collection->removeItemByKey($key);
