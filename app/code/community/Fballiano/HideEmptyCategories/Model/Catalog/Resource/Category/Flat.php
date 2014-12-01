1dcad6d9dcd7e6d9d299eb01cf066a3b9fd91577
Fabrizio Balliano <fabrizio@fabrizioballiano.it>
2014-12-01 10:39:20 +0100

    issue #3

--- app/code/community/Fballiano/HideEmptyCategories/Model/Catalog/Resource/Category/Flat.php
+++ app/code/community/Fballiano/HideEmptyCategories/Model/Catalog/Resource/Category/Flat.php
@@ -1,9 +1,30 @@
 <?php
+/**
+ * FBalliano
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category   FBalliano
+ * @package    Fballiano_HideEmptyCategories
+ * @copyright  Copyright (c) 2014 Fabrizio Balliano (http://fabrizioballiano.it)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 class Fballiano_HideEmptyCategories_Model_Catalog_Resource_Category_Flat extends Mage_Catalog_Model_Resource_Category_Flat
 {
-    protected function _loadNodes($parentNode = null, $recursionLevel = 0, $storeId = 0)
+    protected function _loadNodes($parentNode = null, $recursionLevel = 0, $storeId = 0, $onlyActive = true)
     {
-        $nodes = parent::_loadNodes($parentNode, $recursionLevel, $storeId);
+        $nodes = parent::_loadNodes($parentNode, $recursionLevel, $storeId, $onlyActive);
         foreach ($nodes as $node) {
             if ($node->getDisplayMode() == "PAGE") continue;
             if ($node->getChildrenCategories()) continue;
--- app/code/community/Fballiano/HideEmptyCategories/Model/Observer.php
+++ app/code/community/Fballiano/HideEmptyCategories/Model/Observer.php
@@ -1,10 +1,45 @@
 <?php
+/**
+ * FBalliano
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category   FBalliano
+ * @package    Fballiano_HideEmptyCategories
+ * @copyright  Copyright (c) 2014 Fabrizio Balliano (http://fabrizioballiano.it)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 class Fballiano_HideEmptyCategories_Model_Observer
 {
+    public function catalogCategoryFlatLoadnodesBefore(Varien_Event_Observer $observer)
+    {
+        $select = $observer->getEvent()->getSelect();
+        $select->columns("display_mode");
+    }
+
+    public function catalogCategoryCollectionLoadBefore(Varien_Event_Observer $observer)
+    {
+        if (Mage::app()->getStore()->isAdmin()) return;
+
+        $collection = $observer->getEvent()->getCategoryCollection();
+        $collection->addAttributeToSelect("display_mode");
+    }
+
     public function catalogCategoryCollectionLoadAfter(Varien_Event_Observer $observer)
     {
-    	if (Mage::app()->getStore()->isAdmin()) return;
-    	
+        if (Mage::app()->getStore()->isAdmin()) return;
+
         $collection = $observer->getEvent()->getCategoryCollection();
         foreach ($collection as $key => $item) {
             if ($item->getEntityTypeId() == 3) {
